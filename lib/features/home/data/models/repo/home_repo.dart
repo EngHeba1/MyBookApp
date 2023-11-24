8b746cabd6170ee432779e01368ba336238c0db4
Dell <eheba0302@gmail.com>
2023-11-24 21:50:43 +0200

    implement fetch newst books

--- lib/features/home/data/models/repo/home_repo.dart
+++ lib/features/home/data/models/repo/home_repo.dart
@@ -4,6 +4,6 @@ import 'package:dartz/dartz.dart';
 import '../book_model/book_model.dart';
 
 abstract class HomeRepo{
- Future<Either<Failure,List<BookModel>>> fetchBestSellerBook();
+ Future<Either<Failure,List<BookModel>>> fetchNewstBook();
  Future<Either<Failure,List<BookModel>>> fetchFetureBooks();
 }
---
+++ lib/features/home/data/models/repo/home_repo_implement.dart
@@ -0,0 +1,39 @@
+import 'package:bookly/features/home/data/errors/failuer.dart';
+
+import 'package:bookly/features/home/data/models/book_model/book_model.dart';
+
+import 'package:dartz/dartz.dart';
+
+import '../../../../../core/uitls/api_service.dart';
+import 'home_repo.dart';
+
+class HomeRepoImplement implements HomeRepo{
+  ApiService apiService;
+
+  HomeRepoImplement(this.apiService);
+
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchFetureBooks() {
+    // TODO: implement fetchFetureBooks
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchNewstBook() async{
+       try {
+         var data=await apiService.get(endPoint:
+         "volumes?Sorting=newest&q=subject:Programming&Filtering=free-ebooks");//return map but i want list so i need to
+           //maping it
+         
+         List<BookModel> books=[];  // i want to return this list
+         
+      for(var item  in data["items"]){   //var item "list "
+      books.add(BookModel.fromJson(item));  //
+     }
+      return right(books);
+       } on Exception catch (e) {
+   return left(ServierFailure());
+       }
+    }
+
+}
